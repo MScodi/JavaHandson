2395b958cbc545ade508eeec19fd5c013140152a
Mohinder <63348263+MScodi@users.noreply.github.com>
2021-07-19 00:37:42 +0530

    Add files via upload
    switch case Handson DOB

---
+++ Assignment1.java
@@ -0,0 +1,127 @@
+package scopes;
+
+import java.util.Scanner;
+public class Assignment1 {
+	public static void main(String[] args)
+	{
+		System.out.println("Enter your DOB");
+		System.out.println("Enter the date");
+		int date=0;
+		Scanner s=new Scanner(System.in);
+		date=s.nextInt();
+		System.out.println("Enter the month");
+		String month="null";
+		month=s.next();
+		System.out.println("Enter the year");
+		int year=0;
+		year=s.nextInt();
+		switch (year) {
+		    case 1990:
+			System.out.println("You are early 90's born");
+			break;
+		    case 1991:
+			System.out.println("You are early 90's born");
+			break;		
+			case 1992:
+				System.out.println("You are early 90's born");
+				break;
+			case 1993:
+				System.out.println("You are early 90's born");
+				break;
+			case 1994:
+				System.out.println("You are early 90's born");
+				break;
+			case 1995:
+				System.out.println("You are late 90's born");
+				break;
+			case 1996:
+				System.out.println("You are late 90's born");
+				break;
+			case 1997:
+				System.out.println("You are late 90's born");
+				break;
+			case 1998:
+				System.out.println("You are late 90's born");
+				break;
+			case 1999:
+				System.out.println("You are late 90's born");
+				break;
+			case 2000:
+				System.out.println("You are late 90's born");
+				break;
+			case 2001:
+				System.out.println("You are new decade born");
+				break;
+			case 2002:
+				System.out.println("You are new decade born");
+				break;
+			case 2003:
+				System.out.println("You are new decade born");
+				break;
+			case 2004:
+				System.out.println("You are new decade born");
+				break;
+			case 2005:
+				System.out.println("You are new decade born");
+				break;
+			case 2006:
+				System.out.println("You are new decade born");
+				break;
+			case 2007:
+				System.out.println("You are new decade born");
+				break;
+			case 2008:
+				System.out.println("You are new decade born");
+				break;
+			case 2009:
+				System.out.println("You are new decade born");
+				break;
+			case 2010:
+				System.out.println("You are new decade born");
+				break;
+			case 2011:
+				System.out.println("You are  recently  born");
+				break;
+			case 2012:
+				System.out.println("You are   recently  born");
+				break;
+			case 2013:
+				System.out.println("You are  recently  born");
+				break;
+			case 2014:
+				System.out.println("You are  recently  born");
+				break;
+			case 2015:
+				System.out.println("You are  recently  born");
+				break;
+			case 2016:
+				System.out.println("You are  recently  born");
+				break;
+			case 2017:
+				System.out.println("You are  recently  born");
+				break;
+			case 2018:
+				System.out.println("You are  recently  born");
+				break;
+			case 2019:
+				System.out.println("You are  recently  born");
+				break;
+			case 2020:
+				System.out.println("You are  recently  born");
+				break;
+			case 2021:
+				System.out.println("You are  recently  born");
+				break;
+					
+
+		default:
+			System.out.println("Invalid year");
+			break;
+		}
+
+		
+		
+		
+	}
+
+}
